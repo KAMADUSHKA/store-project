422719fd892b358ab112a6f3dc2218ad769cd610
KAMADUSHKA <kamadushkachathuranga@gmail.com>
2023-05-31 16:56:20 +0530

    update

--- src/Pages/Cards.js
+++ src/Pages/Cards.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Grid, Paper } from "@mui/material";
+import React from "react";
 
 export default function Cards() {
+
+    const paperStyle = {
+        padding: 8,
+        // maxWidth: { xs: 400, lg: 475 },
+        margin: { xs: 2.5, md: 3 },
+        borderRadius: 8,
+        "& > *": {
+          flexGrow: 1,
+          flexBasis: "50%",
+        },
+      };
+
   return (
-    <div>Cards</div>
-  )
+    <Grid container justifyContent="center" alignItems="center" >
+      <Grid display={"flex"}>
+        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
+        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
+        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
+        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
+        
+      </Grid>
+    </Grid>
+  );
 }
