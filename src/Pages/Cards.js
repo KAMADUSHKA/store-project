6d77bcd82c4914c1631e753192525c8019304ff6
KAMADUSHKA <kamadushkachathuranga@gmail.com>
2023-06-01 13:22:56 +0530

    create cards

--- src/Pages/Cards.js
+++ src/Pages/Cards.js
@@ -1,28 +1,126 @@
-import { Grid, Paper } from "@mui/material";
 import React from "react";
+import {
+  Avatar,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import DigiBill from "../componant/DigiBill.png";
+import SmartPose from "../componant/SmartPose.png";
+import SmartMsg from "../componant/smartMsg.png";
+import CocaCola from "../componant/CocaCola.png";
+import InApp from "../componant/InApp.png";
+import EBCM from "../componant/EBCM.png";
+import V_Cart from "../componant/V_Cart.png";
+import SmartCnt from "../componant/SmartCnt.png";
+import { red } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function Cards() {
+  const data = [
+    {
+      image: DigiBill,
+      alt: "DigiBill",
+      name: "Digi Bill",
+    },
+    {
+      image: SmartPose,
+      alt: "SmartPose",
+      name: "Smart Pose",
+    },
+    {
+      image: SmartMsg,
+      alt: "SmartMsg",
+      name: "Smart Messenger",
+    },
+    {
+      image: CocaCola,
+      alt: "CocaCola",
+      name: "CocaCola Management",
+    },
+    {
+      image: InApp,
+      alt: "InApp",
+      name: "In App",
+    },
+    {
+      image: EBCM,
+      alt: "EBCM",
+      name: "EBCM",
+    },
+    {
+      image: V_Cart,
+      alt: "V_Cart",
+      name: "V-Card",
+    },
+    {
+      image: SmartCnt,
+      alt: "SmartCnt",
+      name: "Smart Connector",
+    },
+  ];
 
-    const paperStyle = {
-        padding: 8,
-        // maxWidth: { xs: 400, lg: 475 },
-        margin: { xs: 2.5, md: 3 },
-        borderRadius: 8,
-        "& > *": {
-          flexGrow: 1,
-          flexBasis: "50%",
-        },
-      };
+  const cardData = (cardData, key) => (
+    <Grid key={key} item xs={4} sm={3} md={2.4} lg={2} xl={1.5}>
+      <Card sx={{ maxWidth: 345 }} elevation={10}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ height:35 }} aria-label="recipe" src="https://dsuabgmmtxmj1.cloudfront.net/companyweb/AdeonaLogo.png">
+              R
+            </Avatar>
+          }
+          title="Shrimp and Chorizo Paella  "
+          subheader="September 14, 2016"
+        />
+
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="200"
+            image={cardData.image}
+            alt={cardData.alt}
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. 
+            </Typography>
+          </CardContent>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
+
+  //======//
 
   return (
-    <Grid container justifyContent="center" alignItems="center" >
-      <Grid display={"flex"}>
-        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
-        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
-        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
-        <Paper elevation={10} sx={paperStyle}>efdcdewfed</Paper>
-        
-      </Grid>
-    </Grid>
+    <>
+      <Container>
+        <Grid container spacing={2} marginTop={3}>
+          {data.map((card, key) => cardData(card, key))}
+        </Grid>
+      </Container>
+  
+      
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
   );
 }
--- src/Pages/SmartMsg.js
+++ src/Pages/SmartMsg.js
@@ -1,134 +1,52 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Item>Item</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Item</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Item</Item>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+  
 
 export default function SmartMsg() {
-  const [value, setValue] = React.useState('1'); 
-
-  const handleChange = (event, newValue) => {
-      setValue(newValue);
-  }
-
-  const [docType, setDocType] = React.useState('');
-
-  const [truble, setTruble] = React.useState('');
-
-  const item1Change = (event) => {
-      setDocType(event.target.value);
-  }
-
-  const item2Change = (event) => {
-      setTruble(event.target.value);
-  }
+  
 
 return (
-  <div id='tabel'>
-
-      <Box sx={{ width: '100%', typography: 'body1' }}>
-          <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                  <TabList onChange={handleChange} aria-label="lab API tabs example">
-                      <Tab label="Log Paths" value="1" />
-                      <Tab label="Services" value="2" />
-                      <Tab label="Scripts" value="3" />
-                      <Tab label="Database" value="4" />
-                      <Tab label="Documentation" value="5" />
-                      <Tab label="Troubleshooting Tips" value="6" />
-                  </TabList>
-              </Box>
-              
-                  <TabPanel value="1">
-                      <span>Service : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Server IP : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Server Path : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Description : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                  </TabPanel>
-
-                  <TabPanel value="2">
-                      <span>Service Name : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Server IP : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Servis Path : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Related Commands : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                  </TabPanel>
-
-                  <TabPanel value="3">
-                      <span>location : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Server IP : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <input type="file" /><br/><br/>
-                      <span>Description : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                  </TabPanel>
-
-                  <TabPanel value="4">
-                      <span>DB Naame : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>DB IP/Service IP : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Backup location : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Description : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                  </TabPanel>
-
-                  <TabPanel value="5">
-                      <span>Document Title : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Upload File : 
-
-                              <FormControl variant="standard" sx={{ m: 1, minWidth: 140 }}>
-                                      <InputLabel id="demo-simple-select-standard-label">Document Type</InputLabel>
-
-                                          <Select
-                                              labelId="demo-simple-select-standard-label"
-                                              id="demo-simple-select-standard"
-                                              value={docType}
-                                              onChange={item1Change}
-                                              label="Age"
-                                          >
-                                      
-                                      <MenuItem value={10}>Ten</MenuItem>
-                                      <MenuItem value={20}>Twenty</MenuItem>
-                                      
-                                      </Select>
-                              </FormControl>
-
-                      </span><br/><br/>
-                      <input type="file" /><br/><br/>
-                      <span>Description : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                  </TabPanel>
-
-                  <TabPanel value="6">
-                      <span>Service Name : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Issue Description : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Resolving Procedure : <TextField  id="standard-basic" label="User Name" variant="standard" /> </span><br/><br/>
-                      <span>Upload File : 
-
-                              <FormControl variant="standard" sx={{ m: 1, minWidth: 140 }}>
-                                      <InputLabel id="demo-simple-select-standard-label">Troubleshooting Type</InputLabel>
-
-                                          <Select
-                                              labelId="demo-simple-select-standard-label"
-                                              id="demo-simple-select-standard"
-                                              value={truble}
-                                              onChange={item2Change}
-                                              label="Age"
-                                          >
-                                      
-                                      <MenuItem value={10}>Not</MenuItem>
-                                      <MenuItem value={20}>Mandatory</MenuItem>
-                                      
-                                      </Select>
-                              </FormControl>
-
-                      </span><br/><br/>
-                  </TabPanel>
-          </TabContext>
-      </Box>
-
-  </div>
+    <Box sx={{ flexGrow: 1 }}>
+    <Grid container spacing={1}>
+      <Grid container item spacing={3}>
+        <FormRow />
+      </Grid>
+      <Grid container item spacing={3}>
+        <FormRow />
+      </Grid>
+      <Grid container item spacing={3}>
+        <FormRow />
+      </Grid>
+    </Grid>
+  </Box>
 );
 };
 
