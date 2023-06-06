d99f745a3237269aff6fb90606243c5301820c01
KAMADUSHKA <kamadushkachathuranga@gmail.com>
2023-06-06 10:43:11 +0530

    create add card page

--- src/Pages/AddCard.js
+++ src/Pages/AddCard.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
+import React from "react";
+import ProfileData from "../MUI/CardImage";
+import CardData from "../MUI/CardData";
 
 export default function AddCard() {
   return (
-    <div>AddCard</div>
-  )
+    <>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="lg" sx={{ padding: 6 }}>
+          <Stack spacing={3}>
+            <div>
+              <Typography variant="h4">Add Card</Typography>
+              <br />
+            </div>
+            <div>
+              <Grid
+                container
+                spacing={2}
+                sx={{ justifyContent: "space-around" }}
+              >
+                <Grid xs={12} sm={5} md={4} lg={3} xl={2} margin={2}> 
+                  <ProfileData />
+                </Grid>
+                <Grid xs={12} sm={5} md={6} lg={6} xl={8} margin={2}>
+                  < CardData />
+                </Grid>
+              </Grid>
+            </div>
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
 }
--- src/Pages/SmartMsg.js
+++ src/Pages/SmartMsg.js
@@ -1,86 +1,9 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import { Avatar } from "@mui/material";
+import { Box, Button, Card, CardContent, CardHeader, Divider, Grid, TextField } from '@mui/material'
+import React from 'react'
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 export default function SmartMsg() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          
-          <Avatar
-            sx={{ height: 55, width: 55 }}
-            aria-label="recipe"
-            src="https://dsuabgmmtxmj1.cloudfront.net/companyweb/adeona_new_logo_circle.png"
-          />
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" }, fontFamily: "'Great Vibes', cursive" }}
-          >
-            Adeona Technogy
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    <>652652</>
+  )
 }
+
